838df8b78431f5dbd735468bb81ed37e4880faa8
Simon Bennett <simon@bennett.im>
2013-10-23 19:47:38 +0100

    Think I have fixed openssl issue

--- README.md
+++ README.md
@@ -2,3 +2,17 @@ Install-Pthreads-PHP
 ====================
 
 How I install Pthreads with PHP on my test servers. I will admit this might not be suitable for other users. 
+
+This helps me when I need to set machines up for my Final major project at UNI. I got fed up of having to put them all in one by one. 
+
+
+The Command
+===========
+
+
+Issues
+------
+
+Well to be honest this has being build for my needs. The install is very basic it does not handel questions well like mysql server passwords etc (you might even want it)
+
+PHP tests on slow machines too take a while :( but I would ask people to submit the data at the end as it all helps :) 
--- install.sh
+++ install.sh
@@ -7,15 +7,22 @@ wget http://uk1.php.net/distributions/php-5.4.15.tar.bz2
 apt-get update
 apt-get upgrade -yq
 apt-get -yq install bzip2 openssl libxml2 libxml2-dev gcc make autoconf htop sudo git dstat iotop iftop 
-apt-get -yq build-dep php5
+apt-get -yq build-dep php5 openssl
 tar xvjf php-5*.tar.bz2
 cd php-5*
 cd ext/
 git clone git://github.com/krakjoe/pthreads.git
 cd ../
 ./buildconf --force
-./configure --enable-maintainer-zts --enable-sockets --enable-pcntl --enable-pthreads --with-tsrm-pthreads --with-mysql=mysqlnd --with-mysqli=mysqlnd --with-pgsql=/usr --with-tidy=/usr  --with-openssl=/usr/local/ssl  --with-pdo-pgsql=/usr --with-pdo-mysql=mysqlnd --with-xsl=/usr --enable-zip --with-pear  --with-gd --with-jpeg-dir=/usr --with-png-dir=/usr --with-mcrypt=/usr  --with-config-file-path=/usr/local/lib/
+./configure --enable-maintainer-zts --enable-sockets --enable-pcntl --enable-pthreads --with-tsrm-pthreads --with-mysql=mysqlnd --with-mysqli=mysqlnd --with-pgsql=/usr --with-tidy=/usr  --with-pdo-pgsql=/usr --with-pdo-mysql=mysqlnd --with-xsl=/usr --enable-zip --with-pear  --with-gd --with-jpeg-dir=/usr --with-png-dir=/usr --with-mcrypt=/usr  --with-config-file-path=/usr/local/lib/
 
 make clean
 make
 make test
+make install
+cd ext/openssl
+mv config0.m4 config.m4
+phpize
+./configure
+make
+make install
